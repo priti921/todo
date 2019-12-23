5f5d04f9f7720ca729ba47b6b6ba85e3f7efff29
Priti <roypriti921@gmail.com>
2019-12-23 15:56:26 +0530

    Updates

---
+++ precache-manifest.ca34b3a42ec05b67c3c2cc9dddea3cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3693b06ab012f31342492d84783eaa3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4e837f12666b611510fb",
+    "url": "/todo/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "bc07e8f214a7c79eb434",
+    "url": "/todo/static/css/main.7e6e2482.chunk.css"
+  },
+  {
+    "revision": "4e837f12666b611510fb",
+    "url": "/todo/static/js/2.51ae1234.chunk.js"
+  },
+  {
+    "revision": "bc07e8f214a7c79eb434",
+    "url": "/todo/static/js/main.1a632868.chunk.js"
+  },
+  {
+    "revision": "fa403113ead66244d282",
+    "url": "/todo/static/js/runtime-main.0fecbaf7.js"
+  }
+]);
